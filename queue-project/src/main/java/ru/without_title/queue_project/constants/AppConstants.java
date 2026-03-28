e053b8716c85f59b2310f6b56ee9f285ff9c732f
srpnsk <s04n02@gmail.com>
2026-03-28 20:45:28 +0300

    so, if we don\'t use enums anymore, we need constants

---
+++ queue-project/src/main/java/ru/without_title/queue_project/constants/AppConstants.java
@@ -0,0 +1,17 @@
+package ru.without_title.queue_project.constants;
+
+public class AppConstants {
+    // Статусы очереди
+    public static final String STATUS_WAITING = "WAITING";
+    public static final String STATUS_PASSED = "PASSED";
+    public static final String STATUS_SKIPPED = "SKIPPED";
+
+    // Типы уведомлений
+    public static final String TYPE_SYSTEM = "SYSTEM";
+    public static final String TYPE_QUEUE = "QUEUE";
+
+    // Статусы уведомлений
+    public static final String NOTIF_PENDING = "PENDING";
+    public static final String NOTIF_SENT = "SENT";
+    public static final String NOTIF_READ = "READ";
+}
