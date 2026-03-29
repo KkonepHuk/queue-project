3489f07f76de716bf740be3af2613028149e33c2
Никитос <nik62807@gmail.com>
2026-03-29 03:47:52 +0300

    delete unnecessary methods

--- queue-project/src/main/java/ru/without_title/queue_project/database/dao/GroupRepository.java
+++ queue-project/src/main/java/ru/without_title/queue_project/database/dao/GroupRepository.java
@@ -4,13 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.without_title.queue_project.database.entities.Group;
 
 import java.util.UUID;
-import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, UUID> {
 
-    // Найти группы по создателю
-    List<Group> findByCreatedBy_UserId(UUID userId);
-
-    // Проверка существования группы по имени
-    boolean existsByName(String name);
 }
