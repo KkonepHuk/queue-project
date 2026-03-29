acb9a35ea5b42e73c707b58c855aeb49601d3fe5
Никитос <nik62807@gmail.com>
2026-03-29 05:35:30 +0300

    add patch and delete requests for users

---
+++ queue-project/src/main/java/ru/without_title/queue_project/dto/request/UserUpdateRequest.java
@@ -0,0 +1,26 @@
+package ru.without_title.queue_project.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserUpdateRequest {
+
+
+    private String firstName;
+    private String lastName;
+
+    public UserUpdateRequest() {}
+
+    public UserUpdateRequest(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+}
