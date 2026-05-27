5ada3276ea54ab70ed456af5c0b7392eb8b2dda3
nikitos <iotwink296@gmail.com>
2026-05-28 01:19:05 +0300

    fix timezone

--- queue-project/src/main/java/ru/without_title/queue_project/QueueProjectApplication.java
+++ queue-project/src/main/java/ru/without_title/queue_project/QueueProjectApplication.java
@@ -1,11 +1,19 @@
 package ru.without_title.queue_project;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class QueueProjectApplication {
 
+	@PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(QueueProjectApplication.class, args);
 	}
